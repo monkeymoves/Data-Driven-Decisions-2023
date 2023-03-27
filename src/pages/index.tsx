08855b7325d304cf0305fbf5e78698481a555478
monkeymoves <lukejjmaggs@gmail.com>
2023-03-27 21:55:49 +0100

    "D3 Main Page"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, We're D3: Data Driven Decisions<span className="fancy">.</span>
           </h1>
           <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <h2>A digital agency for the modern age.</h2>
           <SocialList />
         </div>
       </div>
